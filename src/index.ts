3a1245e47c525533e18b6a5c8d462610a2e60bf2
NicolasNewman <nanewman01@gmail.com>
2022-04-29 13:13:33 -0400

    cleaned up index file

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,18 @@ import events from './events';
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
+//==============================//
+//    Command Initialization    //
+//==============================//
+
 client.commands = new Collection();
 for (const command of commands) {
 	client.commands.set(command.data.name, command);
 }
+
+//==============================//
+//     Event Initialization     //
+//==============================//
 
 for (const event of events) {
 	if (event.once) {
