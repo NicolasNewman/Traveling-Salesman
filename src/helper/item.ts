8e394d0028241670a09dcf5857f92f9c98e21b0d
NicolasNewman <nanewman01@gmail.com>
2022-05-03 11:01:08 -0400

    fixed issues with emojis

--- src/helper/item.ts
+++ src/helper/item.ts
@@ -4,19 +4,19 @@ import { ItemKey, ItemObj } from '../types';
 const Items: { [key in ItemKey]: ItemObj } = {
 	Siera: {
 		idFieldName: Prisma.GuildScalarFieldEnum.sieraId,
-		reaction: '1️⃣',
+		reaction: '0⃣',
 	},
 	Mokamoka: {
 		idFieldName: Prisma.GuildScalarFieldEnum.mokamokaId,
-		reaction: '2️⃣',
+		reaction: '1⃣',
 	},
 	Wei: {
 		idFieldName: Prisma.GuildScalarFieldEnum.weiId,
-		reaction: '3️⃣',
+		reaction: '2⃣',
 	},
 	Rapport: {
 		idFieldName: Prisma.GuildScalarFieldEnum.rapportId,
-		reaction: '4️⃣',
+		reaction: '3⃣',
 	},
 };
 
